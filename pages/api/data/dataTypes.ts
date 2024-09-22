0d66fb7cb4560d7f84a18d3f684d5d03839762dd
MStolen <max@amaxsteele.com>
2024-09-21 19:00:02 -0500

    Create API routes

---
+++ pages/api/data/dataTypes.ts
@@ -0,0 +1,15 @@
+export type UserStore = {
+    id: number;
+    jwt: string;
+}[]
+
+export type EventStore = {
+    id: number;
+    name: string;
+    description: string;
+    start: number;
+    end: number;
+    membersOnly: boolean;
+    totalSeats: number;
+    availableSeats: number;
+  }[];
---
+++ pages/api/events/[eventId]/rsvp.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+// import { useRouter } from 'next/router';
+import { useParams } from 'next/navigation';
+import type { EventStore, UserStore } from "@/pages/api/data/dataTypes";
+import { promises as fs } from 'fs';
+import { console } from 'inspector/promises';
+ 
+type Data = {
+    userID: number;
+    eventID: number;
+  };
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data|null>) {
+  if (req.method === 'POST') {
+    console.log(req.headers);
+    const users:UserStore = JSON.parse(await fs.readFile(process.cwd() + '/pages/api/data/userStore.json', 'utf8'))
+    const eventStore: EventStore = JSON.parse(await fs.readFile(process.cwd() + '/pages/api/data/eventStore.json', 'utf8'));
+    const authType = (req.headers.authorization || '').split(' ')[0];
+    const authBody = (req.headers.authorization || '').split(' ')[1];
+    const eventIdStr = req.query.eventId;
+    const eventId = parseInt(eventIdStr as string, 10);
+    console.log(eventId);
+    // const eventId = parseInt(router.query.eventId as string, 10);
+    console.log(authType);
+    console.log(authBody);
+    console.log(eventId);
+    if (authType === 'Bearer' && authBody) {
+        const userID = users.find(user => user.jwt === authBody)?.id || null;
+        if (userID && eventStore.find(event => event.id === eventId)) {
+            res.status(200).json({ userID: userID , eventID:eventId});
+        }
+    }
+    res.status(400).json(null);
+  }
+    // Handle any other HTTP method
+    res.status(401).json(null);
+}
